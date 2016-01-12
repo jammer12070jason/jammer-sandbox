0d08ddb6b7e25d62a3e3a4c2dda1f4f46bdf764e
jammer12070jason <jammer12070jason@yahoo.com>
2016-01-12 15:40:39 -0700

    committing

---
+++ HelloGit/src/com/jsr/tutorial/validate/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.jsr.tutorial.validate.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Git!");
+
+	}
+
+}
